4a57fa0da1e976945ecae57c01f81da9ca78ad23
Ariana Colon <arianacolon4@gmail.com>
2022-10-27 15:58:53 -0400

    Adding PyPoll_Challenge.

--- PyPoll_Challenge.py
+++ PyPoll_Challenge.py
@@ -6,7 +6,7 @@ import csv
 import os
 
 # Add a variable to load a file from a path.
-file_to_load = os.path.join("..", "Resources", "election_results.csv")
+file_to_load = os.path.join("Resources", "election_results.csv")
 # Add a variable to save the file to a path.
 file_to_save = os.path.join("analysis", "election_analysis.txt")
 
@@ -101,16 +101,19 @@ with open(file_to_save, "w") as txt_file:
          # 6d: Print the county results to the terminal.
         print(county_results)
          # 6e: Save the county votes to a text file.
-       
+        txt_file.write(county_results)
          # 6f: Write an if statement to determine the winning county and get its vote count.
-        if (county_vote_count) > (largest_county_count):
+        if (county_vote_count > largest_county_count):
             largest_county_count = county_vote_count
-            largest_county_count = county_name
+            largest_county_turnout = county_name
     # 7: Print the county with the largest turnout to the terminal.
-
-
+    largest_turnout_summary = (
+        f"-------------------------\n"
+        f"Largest County Turnout: {largest_county_turnout}\n"
+        f"-------------------------\n")
+    print(largest_turnout_summary)
     # 8: Save the county with the largest turnout to a text file.
-
+    txt_file.write(largest_turnout_summary)
 
     # Save the final candidate vote count to the text file.
     for candidate_name in candidate_votes:
